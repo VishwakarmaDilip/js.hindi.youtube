67f3dfe4b6fb73ba042e5f3416ae7f7c004bff3e
Diliip Firan Vishwakarma <155577432+VishwakarmaDilip@users.noreply.github.com>
2024-01-04 20:35:49 +0000

    data type and conversion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer verson/JS
+
+//alert(3+3) // we are using Node JS,  not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log("Dilip");
+
+
+let name = "Dilip" 
+let age = 23
+let isLoggedIn = false
+let state;
+
+
+/*
+premitive data type
+    number => 2 to power 53
+    bigint
+    string => ""
+    boolean => true/false
+    null => standalone value
+    undefined =>
+    symbol => unique
+
+
+object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN 
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"Dilip" => true
+*/
+
+let someNumber = 88
+
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber);
+console.log(typeof (stringSomeNumber));
