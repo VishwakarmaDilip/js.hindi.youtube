7323386b50a2443110b1b7e39ee0b64e40267d6d
Diliip Firan Vishwakarma <155577432+VishwakarmaDilip@users.noreply.github.com>
2024-01-04 21:26:01 +0000

    Discussion on Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,44 @@ let isLoggedIn = 0
 
 let someNumber = 88
 
-let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber);
-console.log(typeof (stringSomeNumber));
+// let stringSomeNumber = String(someNumber)
+// console.log(stringSomeNumber);
+// console.log(typeof (stringSomeNumber));
+
+
+/* **********************************Operation************************************* */
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Dilip"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter); // take MDN refference postfix and prefix.
