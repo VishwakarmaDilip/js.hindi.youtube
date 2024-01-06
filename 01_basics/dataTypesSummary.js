5f5841ae2bf6219d21e199914abad865e0cb7346
Diliip Firan Vishwakarma <155577432+VishwakarmaDilip@users.noreply.github.com>
2024-01-06 20:17:23 +0000

    Stack and Heap Memory

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -9,17 +9,17 @@ Primitive
         6> Symbol (to make any value unique)
         7> BigInt
     Example
-*/      const name = "Dilip"
-        const score = 100
-        const isLoggedIn = false
-        const outsidetemp = null
-        let userEmail;
-         const id = Symbol('123')
-        //or
-        const anotherId = Symbol('123')
-            console.log(id === anotherId)
+// */      const name = "Dilip"
+//         const score = 100
+//         const isLoggedIn = false
+//         const outsidetemp = null
+//         let userEmail;
+//          const id = Symbol('123')
+//         //or
+//         const anotherId = Symbol('123')
+//             console.log(id === anotherId)
 
-        const bigNumber = 3421545655665455565n
+//         const bigNumber = 3421545655665455565n
 /*
 Refference (Non Primitive)
     types
@@ -27,13 +27,49 @@ Refference (Non Primitive)
         2> Objects
         3> Function
     Example
-*/      const heros = ["shaktiman", "naagraj", "doga"];
-        let myObj ={
-            name : "Dilip",
-            age :22,
-        }
-        const myFunction = function() {
-            console.log("Helllo world");
-        }
+*/      //const heros = ["shaktiman", "naagraj", "doga"];
+        // let myObj ={
+        //     name : "Dilip",
+        //     age :22,
+        // }
+        // const myFunction = function() {
+        //     console.log("Helllo world");
+        // }
+        // console.log(myFunction());
 
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/* 
+Memory
+    1> Stack
+        use in primitive data type
+        copy
+    2>Heap
+        use in non primitive data type
+        reference
+*/
+// example
+    // 1> stack
+    let myYoutubeName = "just Nonsense Talk"
+    let anathoreName = myYoutubeName
+
+    anathoreName = "Cine Story"
+
+    console.log(myYoutubeName);
+    console.log(anathoreName);
+    
+    // 2> Heap
+    let userOne = {
+        email : "user@gmail.com",
+        upi : "user@paytm",
+    }
+
+    let userTwo = userOne
+
+    userTwo.email = "Dilip@gmail.com"
+
+    console.log(userOne)
+    console.log(userTwo)
 
